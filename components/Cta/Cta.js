c6e33c95022988985f7a08585bb07e600cc70aa4
sebagruia <blackthorus@gmail.com>
2021-06-14 22:20:09 +0300

    Add AboutPresentationSection, Stage component

--- components/Cta/Cta.js
+++ components/Cta/Cta.js
@@ -1,13 +1,16 @@
 import styles from './Cta.module.scss';
+import React from 'react';
 import Image from 'next/image';
 
-const Cta = ({ children }) => {
+const Cta = React.forwardRef(({ onClick, href, children }, ref) => {
   return (
-    <div className={styles.cta}>
-      <p>{children}</p>
-      <Image src="/svg/learnMore_cta.svg" width={12} height={12} alt="next arrow icon" />
+    <div onClick={onClick} ref={ref}>
+      <a className={styles.cta} onClick={onClick} href={href} >
+        <p>{children}</p>
+        <Image src="/svg/learnMore_cta.svg" width={12} height={12} alt="next arrow icon" />
+      </a>
     </div>
   );
-};
+});
 
 export default Cta;
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,11 +1,15 @@
 import styles from './About.module.scss';
 import Layout from '../../components/Layout/Layout';
 import AboutHeroSection from './components/AboutHeroSection/AboutHeroSection';
+import DownloadBanner from '../../components/DownloadBanner/DownloadBanner';
+import AboutPresentationSection from './components/AboutPresentationSection/AboutPresentationSection';
 
 const About = () => {
   return (
     <Layout>
       <AboutHeroSection />
+      <AboutPresentationSection />
+      <DownloadBanner />
     </Layout>
   );
 };
