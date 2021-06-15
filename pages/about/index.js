9540019ac9b0b32ef52535467b576a64b1f77850
sebagruia <blackthorus@gmail.com>
2021-06-15 11:27:43 +0300

    Add DidYouKnowSection component

--- pages/about/index.js
+++ pages/about/index.js
@@ -3,12 +3,14 @@ import Layout from '../../components/Layout/Layout';
 import AboutHeroSection from '../../components/AboutHeroSection/AboutHeroSection';
 import DownloadBanner from '../../components/DownloadBanner/DownloadBanner';
 import AboutPresentationSection from '../../components/AboutPresentationSection/AboutPresentationSection';
+import DidYouKnowSection from '../../components/DidYouKnowSection/DidYouKnowSection';
 
 const About = () => {
   return (
     <Layout>
       <AboutHeroSection />
       <AboutPresentationSection />
+      <DidYouKnowSection />
       <DownloadBanner />
     </Layout>
   );
