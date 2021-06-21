45890111e5d1eaed752d456c54310e03d8d69cb3
sebagruia <blackthorus@gmail.com>
2021-06-21 17:12:42 +0300

    Add Contact page

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -2,10 +2,10 @@ import styles from './Button.module.scss';
 import React from 'react';
 
 const Button = React.forwardRef(({ onClick, href, ...props }, ref) => {
-  const { background, border, textColor, children } = props;
+  const { background, border, textColor, type, children } = props;
   return (
     <a onClick={onClick} href={href} ref={ref}>
-      <button className={`${styles.button} ${styles[background]} ${styles[border]} ${styles[textColor]}`}>
+      <button type={type} className={`${styles.button} ${styles[background]} ${styles[border]} ${styles[textColor]}`}>
         <p className="buttonText">{children}</p>
       </button>
     </a>
