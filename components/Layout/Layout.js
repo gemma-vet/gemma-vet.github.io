4c6bc9dc81058dc3b088d46a9ce169c672bf1d64
sebagruia <blackthorus@gmail.com>
2021-06-25 13:22:21 +0300

    Set up firebase analytics, add analytics events for Page Views and AppButtons clicks

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,10 +1,22 @@
 import styles from './Layout.module.scss';
+import { useEffect } from 'react';
+import firebase from 'firebase/app';
+import initFirebase from '../../firebase';
+import 'firebase/analytics';
 import Head from 'next/head';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import DownloadBanner from '../DownloadBanner/DownloadBanner';
 
+initFirebase();
+
 const Layout = ({ title, textOne, lightGreen, textWhite, children }) => {
+  useEffect(() => {
+    // if (typeof window !== 'undefined' && typeof window.navigator !== 'undefined') {
+      const analytics = firebase.analytics();
+      analytics.logEvent('page_view');
+    // }
+  });
   return (
     <main className={styles.container}>
       <Head>
--- firebase.js
+++ firebase.js
@@ -2,15 +2,19 @@ import firebase from 'firebase/app';
 import 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCkO6a3H-c568rAjHgz8BPaQDo8DblR5T8',
-  authDomain: 'gemma-f1dc7.firebaseapp.com',
-  projectId: 'gemma-f1dc7',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: 'gemma-f1dc7.appspot.com',
   messagingSenderId: '1034583642308',
   appId: '1:1034583642308:web:505c8bc217f82a91c17991',
   measurementId: 'G-JFXER1MP6Z',
 };
 
-firebase.initializeApp(firebaseConfig);
+const initFirebase = () => {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+};
 
-export const firebaseAnalytics = firebase.analytics();
+export default initFirebase;
