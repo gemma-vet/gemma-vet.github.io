8024920fa8bb3ad08485799834828e82944d85b1
sebagruia <blackthorus@gmail.com>
2021-06-29 09:41:09 +0300

    Connected Sendgrid package to Contact form

---
+++ pages/api/contact.js
@@ -0,0 +1,27 @@
+// Sendgrid Mail Sender configuration
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.EMAIL_SENDGRID_API_BLACKTHORUS);
+
+export default async (req, res) => {
+  const { name, email, message } = req.body;
+  const msg = {
+    to: 'blackthorus@gmail.com',
+    from: 'blackthorus@gmail.com',
+    subject: 'New mail from contact form on gemma.vet',
+    text: ` Somebody sent a message through the contact form on www.gemma.vet
+    Name:${name} 
+    Email:${email} 
+    Message:${message}`,
+  };
+  try {
+    await sgMail.send(msg);
+    res.json({ message: `Email has been sent` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error sending email' });
+
+    if (error.response) {
+      console.error(error.response.body);
+    }
+  }
+};
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,31 +1,70 @@
 import styles from './Contact.module.scss';
+import { useState } from 'react';
 import Image from 'next/image';
 import Layout from '../../components/Layout/Layout';
 import SectionBanner from '../../components/SectionBanner/SectionBanner';
 import Button from '../../components/Button/Button';
 
 const Contact = () => {
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
   const bannerInfo = {
     preTitle: 'Contact US',
     title: 'So Nice to Meet You',
+  };
+  const handleOnChange = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = async () => {
+    try {
+      await fetch('/api/contact', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(data),
+      });
+    } catch (error) {
+      console.log(`Error sending email ${error}`);
+    }
   };
   return (
     <Layout title="Contact" lightGreen textWhite>
       <SectionBanner bannerInfo={bannerInfo} />
       <div className={styles.contact}>
         <div className={styles.contactContainer}>
-          <form className={styles.contactForm}>
+          <form onSubmit={handleSubmit} className={styles.contactForm}>
             <label htmlFor="name">Your Name:</label>
-            <input id="name" type="text" name="name" placeholder="Enter your name" />
+            <input
+              onChange={handleOnChange}
+              id="name"
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              value={data.name}
+              required
+            />
             <label htmlFor="email">Your Email:</label>
-            <input id="email" type="email" name="email" placeholder="Enter your email" />
+            <input
+              onChange={handleOnChange}
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              value={data.email}
+              required
+            />
             <label htmlFor="message">Your Message:</label>
             <textarea
-              id="textarea"
-              name="textarea"
+              onChange={handleOnChange}
+              id="message"
+              name="message"
               placeholder="
             How Can We Help? 
             Have Any Feedback?"
+              value={data.message}
+              required
             />
             <div className={styles.buttonContainer}>
               <Button background="darkGreen" border="borderTransparent" textColor="textWhite" type="submit">
