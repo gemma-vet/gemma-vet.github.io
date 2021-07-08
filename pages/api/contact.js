59d65b9095c4e0a466df0e615c29e67672f071e6
sebagruia <blackthorus@gmail.com>
2021-07-08 11:16:08 +0300

    Updated email adress in Contact and FooterContact components

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -16,8 +16,8 @@ const contactApi =  async (req, res) => {
   }
 
   const msg = {
-    to: 'blackthorus@gmail.com',
-    from: 'blackthorus@gmail.com',
+    to: 'accounts@gemma.vet',
+    from: 'accounts@gemma.vet',
     subject: 'New mail from contact form on gemma.vet',
     text: ` Somebody sent a message through the contact form on www.gemma.vet
       Name: ${name} 
