4daea7d9f21fbd6685897529f03921d353c4906f
sebagruia <blackthorus@gmail.com>
2021-07-08 09:41:39 +0300

    Revert to using Image component instead of traditional <img> in App Button component due to compatibility issue

--- components/AppButton/AppButton.js
+++ components/AppButton/AppButton.js
@@ -16,20 +16,18 @@ const AppButton = ({ buttonInfo }) => {
     <Fragment>
       <Link onClick={handleClick} href={href}>
         <a className={styles.desktopVersion}>
-          {/* <Image
+          <Image
             src={src}
             width={dimensions.desktop.width}
             height={dimensions.desktop.height}
             alt={alt}
             layout="fixed"
-          /> */}
-          <img src={src} alt={alt} />
+          />
         </a>
       </Link>
       <Link onClick={handleClick} href={href}>
         <a className={styles.mobileVersion}>
-          {/* <Image src={src} width={dimensions.mobile.width} height={dimensions.mobile.height} alt={alt} layout="fixed" /> */}
-          <img src={src} width={dimensions.mobile.width} height={dimensions.mobile.height} alt={alt} />
+          <Image src={src} width={dimensions.mobile.width} height={dimensions.mobile.height} alt={alt} layout="fixed" />
         </a>
       </Link>
     </Fragment>
