a2bf7e520e2c5a8cd5286861e99c660f66abc72e
sebagruia <blackthorus@gmail.com>
2021-08-09 12:09:11 +0300

    Updated text on About page

--- components/AboutHeroSection/AboutHeroSection.js
+++ components/AboutHeroSection/AboutHeroSection.js
@@ -10,8 +10,8 @@ const AboutHeroSection = () => {
       <div className={styles.aboutHeroContainer}>
         <div className={styles.aboutHeroDescription}>
           <p className={styles.description}>
-            With vets’ mission in mind and <br /> pets’ best interest at heart, <br /> we created Gemma, a smart, <br />{' '}
-            secure, and easy-to-use mobile <br /> app tailored to your needs.
+            With vets’ mission in mind and <br /> pets’ best interest at heart <br /> we created Gemma: <br /> a smart,{' '}
+            secure, <br /> and easy-to-use mobile app.
           </p>
           <Link href="/howItWorks" passHref>
             <Button background="white" border="borderTransparent" textColor="textGreen" type="button">
